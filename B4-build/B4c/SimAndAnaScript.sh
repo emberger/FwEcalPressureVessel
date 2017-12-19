678167d67ad504121c08c470e6f2863c125b39e6
Lorenz Emberger <lorenz@lorenzens-mbp.speedport.ip>
2017-12-19 19:27:26 +0100

    .

--- B4-build/B4c/SimAndAnaScript.sh
+++ B4-build/B4c/SimAndAnaScript.sh
@@ -25,102 +25,102 @@ foldername=AngReso_IT20_OT20_Ogapfirst_25Inner_50Outer_lead1mm_Polystyrene5mm_14
 
  mkdir ~/Geant4/Data/ForwardEcalWithPressureVessel/AngularResolution_OutlierRejection/$foldername
 
- sed -i "19 s%^/run/beamOn.*%/run/beamOn $eventsPerEnergy%" GeantSim.mac
- sed -i "15 s%^/gun/position.*%/gun/position $XPos $YPos $ZPos mm%" GeantSim.mac
-
-     while [ $AngCOUNTER -lt $AngIiterations ]; do
-
-         sed -i "16 s%^/gun/direction.*%/gun/direction $Xangle $Yangle $Zangle%" GeantSim.mac
-
-         EnCOUNTER=0
-         en=$Sen
-         buf=start
-         sed -i "18 s%^/gun/energy.*%/gun/energy $buf MeV%" GeantSim.mac
-
-         while [  $EnCOUNTER -lt $EnIterations ]; do
-
-             s=$foldername
-
-             sed -i "18 s%$buf%$en%" GeantSim.mac
-             buf=$en
-
-             ./exampleB4c -m GeantSim.mac
-
-             s+=/gamma
-             s+=$en
-             s+=MeV
-             s+=_$Xangle
-             s+=:$Yangle
-             s+=_.root
-
-             #echo $s
-
-             mv Tree.root ~/Geant4/Data/ForwardEcalWithPressureVessel/AngularResolution_OutlierRejection/$s
-
-             let en=en+$increment
-             let EnCOUNTER=EnCOUNTER+1
-         done
-
-
-         Xangle=$(echo "($Xangle+$XAngIncrement)" | bc)
-         Yangle=$(echo "($Yangle+$YAngIncrement)" | bc)
-
-         let AngCOUNTER=AngCOUNTER+1
- done
-
-AngCOUNTER=0
-Sen=1500
-EnIterations=1
-increment=500
-eventsPerEnergy=5000
-
-Xangle=0.4     #starting angles
-Yangle=0.4
-Zangle=1.0
-
-sed -i "19 s%^/run/beamOn.*%/run/beamOn $eventsPerEnergy%" GeantSim.mac
-
-    while [ $AngCOUNTER -lt $AngIiterations ]; do
-
-        sed -i "16 s%^/gun/direction.*%/gun/direction $Xangle $Yangle $Zangle%" GeantSim.mac
-
-
-        EnCOUNTER=0
-        en=$Sen
-        buf=start
-        sed -i "18 s%^/gun/energy.*%/gun/energy $buf MeV%" GeantSim.mac
-
-        while [  $EnCOUNTER -lt $EnIterations ]; do
-
-            s=$foldername
-
-            sed -i "18 s%$buf%$en%" GeantSim.mac
-            buf=$en
-
-            ./exampleB4c -m GeantSim.mac
-
-            s+=/gamma
-            s+=$en
-            s+=MeV
-            s+=_$Xangle
-            s+=:$Yangle
-            s+=_.root
-
-            #echo $s
-
-            mv Tree.root ~/Geant4/Data/ForwardEcalWithPressureVessel/AngularResolution_OutlierRejection/$s
-
-            let en=en+$increment
-            let EnCOUNTER=EnCOUNTER+1
-        done
-
-
-        Xangle=$(echo "($Xangle+$XAngIncrement)" | bc)
-        Yangle=$(echo "($Yangle+$YAngIncrement)" | bc)
-
-        let AngCOUNTER=AngCOUNTER+1
-done
-
+#  sed -i "19 s%^/run/beamOn.*%/run/beamOn $eventsPerEnergy%" GeantSim.mac
+#  sed -i "15 s%^/gun/position.*%/gun/position $XPos $YPos $ZPos mm%" GeantSim.mac
+#
+#      while [ $AngCOUNTER -lt $AngIiterations ]; do
+#
+#          sed -i "16 s%^/gun/direction.*%/gun/direction $Xangle $Yangle $Zangle%" GeantSim.mac
+#
+#          EnCOUNTER=0
+#          en=$Sen
+#          buf=start
+#          sed -i "18 s%^/gun/energy.*%/gun/energy $buf MeV%" GeantSim.mac
+#
+#          while [  $EnCOUNTER -lt $EnIterations ]; do
+#
+#              s=$foldername
+#
+#              sed -i "18 s%$buf%$en%" GeantSim.mac
+#              buf=$en
+#
+#              ./exampleB4c -m GeantSim.mac
+#
+#              s+=/gamma
+#              s+=$en
+#              s+=MeV
+#              s+=_$Xangle
+#              s+=:$Yangle
+#              s+=_.root
+#
+#              #echo $s
+#
+#              mv Tree.root ~/Geant4/Data/ForwardEcalWithPressureVessel/AngularResolution_OutlierRejection/$s
+#
+#              let en=en+$increment
+#              let EnCOUNTER=EnCOUNTER+1
+#          done
+#
+#
+#          Xangle=$(echo "($Xangle+$XAngIncrement)" | bc)
+#          Yangle=$(echo "($Yangle+$YAngIncrement)" | bc)
+#
+#          let AngCOUNTER=AngCOUNTER+1
+#  done
+#
+# AngCOUNTER=0
+# Sen=1500
+# EnIterations=1
+# increment=500
+# eventsPerEnergy=5000
+#
+# Xangle=0.4     #starting angles
+# Yangle=0.4
+# Zangle=1.0
+#
+# sed -i "19 s%^/run/beamOn.*%/run/beamOn $eventsPerEnergy%" GeantSim.mac
+#
+#     while [ $AngCOUNTER -lt $AngIiterations ]; do
+#
+#         sed -i "16 s%^/gun/direction.*%/gun/direction $Xangle $Yangle $Zangle%" GeantSim.mac
+#
+#
+#         EnCOUNTER=0
+#         en=$Sen
+#         buf=start
+#         sed -i "18 s%^/gun/energy.*%/gun/energy $buf MeV%" GeantSim.mac
+#
+#         while [  $EnCOUNTER -lt $EnIterations ]; do
+#
+#             s=$foldername
+#
+#             sed -i "18 s%$buf%$en%" GeantSim.mac
+#             buf=$en
+#
+#             ./exampleB4c -m GeantSim.mac
+#
+#             s+=/gamma
+#             s+=$en
+#             s+=MeV
+#             s+=_$Xangle
+#             s+=:$Yangle
+#             s+=_.root
+#
+#             #echo $s
+#
+#             mv Tree.root ~/Geant4/Data/ForwardEcalWithPressureVessel/AngularResolution_OutlierRejection/$s
+#
+#             let en=en+$increment
+#             let EnCOUNTER=EnCOUNTER+1
+#         done
+#
+#
+#         Xangle=$(echo "($Xangle+$XAngIncrement)" | bc)
+#         Yangle=$(echo "($Yangle+$YAngIncrement)" | bc)
+#
+#         let AngCOUNTER=AngCOUNTER+1
+# done
+#
 
 FILES=~/Geant4/Data/ForwardEcalWithPressureVessel/AngularResolution_OutlierRejection/$foldername
 ANALYSIS=$FILES/Analysis
